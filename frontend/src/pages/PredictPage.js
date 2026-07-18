c903d5b809400140a31911c96b962534da3ee808
@VinayPanchal <43291965+vinaypanchal007@users.noreply.github.com>
2026-07-18 20:54:34 +0530

    Update PredictPage.js

--- frontend/src/pages/PredictPage.js
+++ frontend/src/pages/PredictPage.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = "https://node-server-8r8k.onrender.com";
 
 const numberFields = [
   "Age",
